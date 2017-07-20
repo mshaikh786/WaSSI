21c381da5c83f6805946ba24c874f090a4c7ba31
Mohsin Ahmed Shaikh <mohsinshaikh786@gmail.com>
2017-07-20 14:17:19 +0800

    made buffer for ARGS a bit bigger to get longer paths to input and output files

--- code/GENERAL.f90
+++ code/GENERAL.f90
@@ -146,8 +146,8 @@ REAL,POINTER:: RUNLAND(:,:,:,:),ETLAND(:,:,:,:),GEPLAND(:,:,:,:)
       DATA MONTHL/31,29,31,30,31,30,31,31,30,31,30,31/
       
 ! --- For reading in the command line arguments 
-      CHARACTER(len=32),ALLOCATABLE:: ARGS(:) 
-      CHARACTER(len=32) ARCH,INPATH,OUTPATH
+      CHARACTER(len=256),ALLOCATABLE:: ARGS(:) 
+      CHARACTER(len=256) ARCH,INPATH,OUTPATH
       INTEGER (kind=4) iargc,INDX
 ! --- Write introductory information to screen
       
@@ -315,7 +315,8 @@ WRITE(*,*) 'OUTPUT files will be written in directory ',TRIM(OUTPATH)
       ALLOCATE (RUNLAND(NGRID,NYEAR,12,31))
       ALLOCATE (ETLAND(NGRID,NYEAR,12,31))
       ALLOCATE (GEPLAND(NGRID,NYEAR,12,31))
-
+        
+        print*,"Entering Loops"
 !$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(ICELL,IYEAR,IM,MNDAY)	      
 	DO 200 ICELL=1,NGRID
 
